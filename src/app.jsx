98cf75108edcae4a42fa486271418acfdf072fb7
kawshan <kawshan.virantha9999@gmail.com>
2025-06-03 16:27:32 +0530

    nav bar is fully completed

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
         <>
-        <Hero/>
+            <NavBar/>
+            <Hero/>
             <ShowcaseSection/>
         </>
     )
