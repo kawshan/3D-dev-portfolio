f74525198d296c130d03deb206f391ff1b6bd71d
kawshan <kawshan.virantha9999@gmail.com>
2025-06-03 15:52:49 +0530

    finished showcase section

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
 const App = () => {
     return (
         <>
         <Hero/>
+            <ShowcaseSection/>
         </>
     )
 }
