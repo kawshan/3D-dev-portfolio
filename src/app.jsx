45f229449ebe91a154dfccc6f3c2d1eca4d5b29c
kawshan <kawshan.virantha9999@gmail.com>
2025-06-18 15:04:30 +0530

    marquee is finished ts 1:48:01

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <NavBar/>
             <Hero/>
             <ShowcaseSection/>
+            <LogoSection />
         </>
     )
 }
